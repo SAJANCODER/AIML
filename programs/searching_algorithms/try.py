5e18b6018686db5de3941c9c30c96c2eb28520a4
unknown <sajanneyveli@gmail.com>
2025-04-20 15:15:04 +0530

    updated bfs

--- programs/searching_algorithms/try.py
+++ programs/searching_algorithms/try.py
@@ -1,29 +1,26 @@
-#implementing breadth first search
-
-from collections import deque
+from collections import deque #deque stands for double ended adding nodes
 def bfs(graph,start):
-    visited = set([start])
+    visited= set([start])
     queue = deque([start])
-
     print("BFS TRAVERSAL")
-    while queue: #node accessed
-        node = queue.popleft()   #pop the left value in queue
-        print(node,end=" ")
-
+    #since i need to visit the queue , so i need a looping statement to visit until the end
+    while queue:
+        #i have started with a node , so i need to pop the node which i have started with
+        node = queue.popleft()
+        print(node,end=" ") #print the current node
         for neighbor in graph.get(node,set()):
-            if neighbor not in visited :
+            if neighbor not in visited:
                 visited.add(neighbor)
                 queue.append(neighbor)
-def create_graph():
-    graph={}  #initilization of graph values
-    print("create your own graph!")
+def creat_graph():
+    graph={} #initilize the graph, as per the user.
     while True:
-        node = input("Enter the node of the graph (Done to stop):").strip().upper()
+        node = input("enter the node : (DONE) to exit:").strip().upper()
         if not node:
             continue
         if node == 'DONE':
             break
-        neighbors = input(f"Enter the nighbours of {node} 's node:").upper().split()
+        neighbors=input(f"enter the {node} neighbor:").upper().split()
         if node not in graph:
             graph[node]=set()
         graph[node].update(neighbors)
@@ -31,22 +28,19 @@ def create_graph():
         for neighbor in neighbors:
             if neighbor not in graph:
                 graph[neighbor]=set()
-            graph[neighbor].add(node)
+                graph[neighbor].add(node)
     return graph
-if __name__ == "__main__":
-    user_graph = create_graph() #creating a object for create_graph
+if __name__=="__main__":
+    user_graph = creat_graph()
     if user_graph:
         while True:
-            start_node=input("enter the start node:").strip().upper()
+            start_node = input("Enter the start node:").upper().strip()
             if start_node in user_graph:
                 break
-            print("not in graph") # this is else part
+            print("user node not in graph")
         bfs(user_graph,start_node)
     else:
-        print("no graph exists")
-
-
-
+        print("no graph exitst")
 
 
 
